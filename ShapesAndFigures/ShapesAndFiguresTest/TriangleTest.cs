38f3903e73faf2280df37cf372320281577e49fd
Zajecc <Zajcik.ez@gmail.com>
2019-09-24 14:48:29 +0300

    TriangleTest

---
+++ ShapesAndFigures/ShapesAndFiguresTest/TriangleTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShapesAndFiguresTest
+{
+    [TestClass]
+    public class TriangleTest
+    {
+        [TestMethod]
+        public void TriangleAreaTest()
+        {
+            double a = 1, b = 1, c = 1;
+            double expetedArea = 0.43301;
+            Triangle t = new Triangle(a, b, c);
+            double actualArea = t.GetArea();
+            Assert.AreEqual(expetedArea, actualArea);
+        }
+
+        [TestMethod]
+        public void TrianglePerimeterTest()
+        {
+            double a = 1, b = 1, c = 1;
+            double expetedPerimeter = 3;
+            Triangle t = new Triangle(a, b, c);
+            double actualPerimeter = t.GetPerimeter();
+            Assert.AreEqual(expetedPerimeter, actualPerimeter);
+        }
+
+    }
+}
