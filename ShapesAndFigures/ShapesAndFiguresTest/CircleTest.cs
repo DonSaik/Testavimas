00e2cd1b3d7f4b10505020098aa387e3f582402c
DonSaik <ds32204@gmail.com>
2019-09-24 14:18:31 +0300

    Circle tests

---
+++ ShapesAndFigures/ShapesAndFiguresTest/CircleTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShapesAndFiguresTest
+{
+    [TestClass]
+    public class CircleTest
+    {
+        [TestMethod]
+        public void PerimeterTest()
+        {
+            double r = 5.5;
+            double expectedResult = 34.557519;
+            Circle c = Circle(r);
+            double result = c.GetPerimeter();
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void AreaTest()
+        {
+            double r = 5.5;
+            double expectedResult = 95.033178;
+            Circle c = Circle(r);
+            double result = c.GetArea();
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
