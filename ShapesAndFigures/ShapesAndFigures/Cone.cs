0124ce157e85c297f9427a18ac9df5d6ca9dec96
DonSaik <ds32204@gmail.com>
2019-10-07 09:45:39 +0300

    cone test

---
+++ ShapesAndFigures/ShapesAndFigures/Cone.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapesAndFigures
+{
+    public class Cone : Figure
+    {
+        public override double GetArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetBasePerimeter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetVolume()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ShapesAndFigures/ShapesAndFiguresTest/ConeTest.cs
+++ ShapesAndFigures/ShapesAndFiguresTest/ConeTest.cs
@@ -1,14 +1,72 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShapesAndFigures;
 
 namespace ShapesAndFiguresTest
 {
     [TestClass]
-    public class UnitTest1
+    public class ConeTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ConeBasePerimeterTest()
         {
+            Cone cone = new Cone( 11.5, 10);
+            double actualPerimeter = cone.GetBasePerimeter();
+            double expectedPerimeter = 72.2566;
+            Assert.AreEqual(expectedPerimeter, actualPerimeter, 0.0001);
+
+        }
+        [TestMethod]
+        public void ConeBasePerimeterWithNegativesTest()
+        {
+            Cone cone = new Cone( -11.5, 10);
+            double actualPerimeter = cone.GetBasePerimeter();
+            double expectedPerimeter = 0;
+            Assert.AreEqual(expectedPerimeter, actualPerimeter, 0.0001);
+
+        }
+        [TestMethod]
+        public void ConeAreaTest()
+        {
+            Cone cone = new Cone( 11.5, 12.5);
+            double actualArea = cone.GetArea();
+            double expectedArea = 1029.1254 ;
+            Assert.AreEqual(expectedArea, actualArea, 0.0001);
+
+        }
+        [DataRow(-1, 2, 0)]
+        [DataRow(1, -1, 0)]
+        [DataRow(0, 1, 0)]
+        [DataRow(1, 0, 0)]
+        [TestMethod]
+        public void ConeAreaWithNegativesTest(double radius, double height, double expecterArea)
+        {
+            Cone cone = new Cone(radius, height);
+            double actualArea = cone.GetArea();
+            Assert.AreEqual(expecterArea, actualArea, 0.0001);
+
+        }
+        [DataRow(-1, 2, 0)]
+        [DataRow(1, -1, 0)]
+        [DataRow(0, 1, 0)]
+        [DataRow(1, 0, 0)]
+        [TestMethod]
+        public void ConeVolumeWithNegativesTest(double radius, double height, double expecterArea)
+        {
+            Cone cone = new Cone(radius, height);
+            double actualArea = cone.GetVolume();
+            Assert.AreEqual(expecterArea, actualArea, 0.0001);
+
+        }
+        [DataRow(11.2, 12.5, 1642.01)]
+        [DataRow(35.9, 0.2, 269.928)]
+        [TestMethod]
+        public void ConeVolumeTest(double radius, double height, double expecterArea)
+        {
+            Cone cone = new Cone(radius, height);
+            double actualArea = cone.GetVolume();
+            Assert.AreEqual(expecterArea, actualArea, 0.01);
+
         }
     }
 }
